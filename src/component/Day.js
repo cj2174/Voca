7bbabeab9819562976897b86fb0c56e930981c22
yunseo <cj2174@naver.com>
2024-10-26 17:45:41 +0900

    CRUD add

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,20 +1,33 @@
 import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
+import Word from "./Word";
+import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
-export default function Day() {
+export default function Day({}) {
   const day = useParams().day;
-  const wordList = dummy.words.filter((word) => word.day === Number(day));
+  const words = useFetch(`http://localhost:3001/words?day=${day}`);
+  // const wordList = dummy.words.filter((word) => word.day === Number(day));
+
+  // const [words, setWords] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(`http://localhost:3001/words?day=${day}`)
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       setWords(data);
+  //     });
+  // },[day]);
 
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map((word) => (
-            <tr key={word.id}>
-              <td>{word.eng}</td>
-              <td>{word.kor}</td>
-            </tr>
+          {words.map((word) => (
+            <Word word={word} key={word.id} />
           ))}
         </tbody>
       </table>
